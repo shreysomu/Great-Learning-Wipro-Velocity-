aad122699fa4ff6e33a94616d1e4e36aba0d09ec
Shrey Trivedi <54596226+shreysomu@users.noreply.github.com>
2022-05-12 03:04:10 +0530

    Create Handson1.java

---
+++ Java/Day1/Handson1.java
@@ -0,0 +1,12 @@
+public class Handson1{
+public static void main(String[] args){
+String Name = "SHREY TRIVEDI";
+int age = 21;
+int weight = 77;
+String address ="Kanpur";
+System.out.println("My Name is " +Name);
+System.out.println("My age is " +age);
+System.out.println("My weight is " +weight);
+System.out.println("My address is " +address);
+}
+}
