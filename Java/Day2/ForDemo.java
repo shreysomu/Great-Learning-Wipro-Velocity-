c992224da0b46b346eacebd4a4a56bbd3c95ff16
Shrey Trivedi <54596226+shreysomu@users.noreply.github.com>
2022-05-12 03:08:41 +0530

    Create ForDemo.java

---
+++ Java/Day2/ForDemo.java
@@ -0,0 +1,9 @@
+class ForDemo{
+        public static void main(String[] args){
+            int[] arr = {1,2,3,4,5};
+            // for(int i=0;i<5;i++)
+          // System.out.println(arr[i]);
+             for(int ele: arr)
+           System.out.println(ele);
+        }
+     }
